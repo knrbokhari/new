250b3fdf00f62969122c135ee5c59d2d1b14cd8d
naeem <kazinaeemrayhan@gmail.com>
2022-02-16 20:44:18 +0600

    2nd commit

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('I am in a separate file')
+function increaseAndDecrease(product, price, isIncreasing) {
+    const number = document.getElementById(product + '-num');
+
+    let productPrice = document.getElementById(product + '-price');
+
+    if (isIncreasing == true) {
+        if (5 > number.value) {
+            const preNum = parseInt(number.value) + 1;
+            number.value = preNum;
+            productPrice.innerText = parseInt(price * number.value);
+        }
+    }
+    else {
+        if (0 < number.value) {
+            const preNum = parseInt(number.value) - 1;
+            number.value = preNum;
+            productPrice.innerText = parseInt(price * number.value);
+        }
+    }
+}
+// iphone
+document.getElementById('iphone-plus').addEventListener('click', function () {
+    increaseAndDecrease('iphone', 1299, true);
+})
+
+document.getElementById('iphone-minus').addEventListener('click', function () {
+    increaseAndDecrease('iphone', 1299, false);
+})
+// iphone case
+document.getElementById('case-plus').addEventListener('click', function () {
+    increaseAndDecrease('case', 59, true);
+})
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    increaseAndDecrease('case', 59, false);
+})
