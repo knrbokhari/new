687c25545cf7e66e370ae271f69608728751851f
naeem <kazinaeemrayhan@gmail.com>
2022-02-21 19:35:22 +0600

    done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// increase And Decrease input value and prices
 function increaseAndDecrease(product, price, isIncreasing) {
     const number = document.getElementById(product + '-num');
 
@@ -17,20 +18,27 @@ function increaseAndDecrease(product, price, isIncreasing) {
             productPrice.innerText = parseInt(price * number.value);
         }
     }
+    totalCost();
 }
-// iphone
-document.getElementById('iphone-plus').addEventListener('click', function () {
-    increaseAndDecrease('iphone', 1299, true);
-})
-
-document.getElementById('iphone-minus').addEventListener('click', function () {
-    increaseAndDecrease('iphone', 1299, false);
-})
-// iphone case
-document.getElementById('case-plus').addEventListener('click', function () {
-    increaseAndDecrease('case', 59, true);
-})
-
-document.getElementById('case-minus').addEventListener('click', function () {
-    increaseAndDecrease('case', 59, false);
-})
+// cost calculation
+function totalCost(){
+    // inputs
+    const iphoneNum = document.getElementById('iphone-num');
+    const caseNum = document.getElementById('case-num');
+    // subTotal, tex, total getting from html
+    const subTotal = document.getElementById('sub-totol');
+    const tax = document.getElementById('tax');
+    const total = document.getElementById('total');
+    // changing values
+    subTotal.innerText = iphoneNum.value * 1299 + caseNum.value * 59;
+    tax.innerText = (parseFloat(subTotal.innerText) * 0.05).toFixed(2);
+    total.innerText = (parseFloat(subTotal.innerText)+ parseFloat(tax.innerText)).toFixed(2);
+}
+// iphone plus
+document.getElementById('iphone-plus').addEventListener('click', () => increaseAndDecrease('iphone', 1299, true));
+// iphone minus
+document.getElementById('iphone-minus').addEventListener('click', () => increaseAndDecrease('iphone', 1299, false));
+// iphone case plus
+document.getElementById('case-plus').addEventListener('click', () => increaseAndDecrease('case', 59, true));
+// iphone case minus
+document.getElementById('case-minus').addEventListener('click', () => increaseAndDecrease('case', 59, false));
